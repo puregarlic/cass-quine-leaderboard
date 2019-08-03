0b00f9546a763d0db7b59fdbec20d286573496e8
Graham Barber <grahammbarber@gmail.com>
2019-08-02 17:47:13 -0700

    fix(leaderboard): use name and language as list key

--- www/components/leaderboard.js
+++ www/components/leaderboard.js
@@ -1,40 +1,42 @@
-import { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import Card from './card'
-import { Table, TableItem, TableHeader } from './table'
+import Card from "./card";
+import { Table, TableItem, TableHeader } from "./table";
 
 export default props => {
-  const [scores, setScores] = useState(props.scores)
+  const [scores, setScores] = useState(props.scores);
 
   const sortScores = type => {
-    setScores([...scores].sort((a, b) => a[type] - b[type]))
-  }
+    setScores([...scores].sort((a, b) => a[type] - b[type]));
+  };
 
   return (
-    <motion.div style={{ gridArea: 'board' }}>
+    <motion.div style={{ gridArea: "board" }}>
       <Card>
-        <h1 style={{ margin: 0, fontSize: '2.5rem', fontWeight: 800 }}>
+        <h1 style={{ margin: 0, fontSize: "2.5rem", fontWeight: 800 }}>
           Quine Challenge Leaderboard
         </h1>
-        <p style={{ fontSize: '0.9rem', color: 'darkGrey' }}>
-          <b style={{ color: 'grey' }}>Not final results.</b> These numbers
+        <p style={{ fontSize: "0.9rem", color: "darkGrey" }}>
+          <b style={{ color: "grey" }}>Not final results.</b> These numbers
           assume a coolness level of 10 and use self-reported numbers, so your
           final rank may be higher or lower due to final testing hardware and
           coolness vote results.
         </p>
         <Table>
           <TableHeader
-            onSortByRank={() => sortScores('rank')}
-            onSortByScore={() => sortScores('score')}
-            onSortByTime={() => sortScores('time')}
-            onSortBySize={() => sortScores('size')}
+            onSortByRank={() => sortScores("rank")}
+            onSortByScore={() => sortScores("score")}
+            onSortByTime={() => sortScores("time")}
+            onSortBySize={() => sortScores("size")}
           />
-          {scores.map((person, index) => {
-            return <TableItem {...person} key={person.name} />
+          {scores.map(person => {
+            return (
+              <TableItem {...person} key={`${person.name}${person.language}`} />
+            );
           })}
         </Table>
       </Card>
     </motion.div>
-  )
-}
+  );
+};
